81cc6ca70483ad37fc9378e8ccb2ac813275246a
Pixelsuft <pixelsuft@gmail.com>
2024-08-20 16:09:15 +0700

    wip

--- home/lexa/.local/share/dwm/autostart.sh
+++ home/lexa/.local/share/dwm/autostart.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 HDD_STR=$(lsblk | grep 1.4T)
 ls /media/HDD/Games &> /dev/null || sudo mount /dev/${HDD_STR:2:4} /media/HDD
+dispwin /home/lexa/.config/color/sm770P.icm &
 picom --backend glx --vsync &
 setxkbmap -option grp:win_space_toggle us,ru &
 feh --bg-fill /home/lexa/Pictures/img19.jpg &
