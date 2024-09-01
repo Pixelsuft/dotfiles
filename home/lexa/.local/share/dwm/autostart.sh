3846c7338600cd323a079564e796240c1e5ac6c4
Pixelsuft <pixelsuft@gmail.com>
2024-09-01 21:16:04 +0700

    fix ntfs rw

--- home/lexa/.local/share/dwm/autostart.sh
+++ home/lexa/.local/share/dwm/autostart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 HDD_STR=$(lsblk | grep 1.4T)
-ls /media/HDD/Games &> /dev/null || sudo mount /dev/${HDD_STR:2:4} /media/HDD
+ls /media/HDD/Games &> /dev/null || sudo ntfs-3g /dev/${HDD_STR:2:4} /media/HDD
 dispwin /home/lexa/.config/color/sm770P.icm &
 picom --backend glx --vsync &
 setxkbmap -option grp:win_space_toggle us,ru &
